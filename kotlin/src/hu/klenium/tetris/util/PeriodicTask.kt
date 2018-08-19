c59065e47db4ced83058e886c383bf276b1d743c
klenium <klenium2@gmail.com>
2018-08-19 21:22:43 +0200

    Implemented PeriodicTask and fixed its tests

--- kotlin/src/hu/klenium/tetris/util/PeriodicTask.kt
+++ kotlin/src/hu/klenium/tetris/util/PeriodicTask.kt
@@ -1,12 +1,24 @@
 package hu.klenium.tetris.util
 
-class PeriodicTask(task: Runnable, delay: Int) {
+import java.util.*
+import kotlin.concurrent.timer
+
+class PeriodicTask(private val task: () -> Unit, delay: Int) {
+    private val time: Long = delay.toLong()
+    private var clock: Timer? = null
+    fun start() {
+        clock = timer(
+            daemon = true,
+            initialDelay = time,
+            period = time,
+            action = { task.invoke() }
+        )
+    }
     fun stop() {
-
+        clock?.cancel()
     }
-
     fun reset() {
-
+        stop()
+        start()
     }
-
 }
--- kotlin/test/PeriodicTaskSlowTest.java
+++ kotlin/test/PeriodicTaskSlowTest.java
@@ -1,5 +1,6 @@
 import helpers.TestUtil;
 import hu.klenium.tetris.util.PeriodicTask;
+import kotlin.Unit;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,17 +12,22 @@ public class PeriodicTaskSlowTest {
     private PeriodicTask clock = null;
     private int foo = 1;
     @BeforeEach void setUp() {
-        clock = new PeriodicTask(() -> ++foo, PERIOD);
+        clock = new PeriodicTask(() -> {
+            ++foo;
+            return Unit.INSTANCE;
+        }, PERIOD);
     }
     @Test void nonStarted() {
         TestUtil.runLater(OFFSET + PERIOD, () -> assertEquals(foo, 1));
     }
     @Test void multiplePeriods() {
+        clock.start();
         TestUtil.runLater(OFFSET, () -> assertEquals(foo, 1));
         TestUtil.runLater(PERIOD, () -> assertEquals(foo, 2));
         TestUtil.runLater(PERIOD, () -> assertEquals(foo, 3));
     }
     @Test void stoped() {
+        clock.start();
         TestUtil.runLater(OFFSET, () -> {
             assertEquals(foo, 1);
             clock.stop();
@@ -30,6 +36,7 @@ public class PeriodicTaskSlowTest {
         TestUtil.runLater(PERIOD, () -> assertEquals(foo, 1));
     }
     @Test void resetPeriodTime() {
+        clock.start();
         TestUtil.runLater(OFFSET, () -> {
             assertEquals(foo, 1);
             clock.reset();
