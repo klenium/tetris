cf1d5f9a03a04289757ef98749200d16f0122278
klenium <klenium2@gmail.com>
2019-04-12 12:51:41 +0200

    Removed unused methods.

--- java/src/main/java/hu/klenium/tetris/util/Point.java
+++ java/src/main/java/hu/klenium/tetris/util/Point.java
@@ -1,7 +1,5 @@
 package hu.klenium.tetris.util;
 
-import java.util.Objects;
-
 public class Point {
     public final int x;
     public final int y;
@@ -11,16 +9,5 @@ public class Point {
     }
     public Point add(Point other) {
         return new Point(this.x + other.x, this.y + other.y);
-    }
-    @Override public boolean equals(Object obj) {
-        if (obj == this)
-            return true;
-        if (obj == null || getClass() != obj.getClass())
-            return false;
-        Point other = (Point) obj;
-        return this.x == other.x && this.y == other.y;
-    }
-    @Override public int hashCode() {
-        return Objects.hash(x, y);
     }
 }
