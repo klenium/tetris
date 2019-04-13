aa072fe5a6d2285b8f674bb1ceaeea63e5c67220
klenium <klenium2@gmail.com>
2019-04-14 00:05:29 +0200

    Added GUI test helpes and test cases for keypress events.

--- java/src/test/java/gui/KeyPressTest.java
+++ java/src/test/java/gui/KeyPressTest.java
@@ -1,9 +1,54 @@
 package gui;
 
 import helpers.TestMainApplication;
+import hu.klenium.tetris.logic.Command;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(ApplicationExtension.class)
 public class KeyPressTest extends TestMainApplication {
+    @Start private void startFxTest(Stage primaryStage) {
+        setUp(primaryStage);
+    }
+    @Test public void sendCommandWnehKeyPressed(FxRobot robot) {
+        bindParams(robot, "");
+        Command recievedCommand;
+        hitKey(KeyCode.W);
+        recievedCommand = game.getLastCommand();
+        assertEquals(Command.ROTATE, recievedCommand);
+        hitKey(KeyCode.A);
+        recievedCommand = game.getLastCommand();
+        assertEquals(Command.MOVE_LEFT, recievedCommand);
+        hitKey(KeyCode.S);
+        recievedCommand = game.getLastCommand();
+        assertEquals(Command.MOVE_DOWN, recievedCommand);
+        hitKey(KeyCode.D);
+        recievedCommand = game.getLastCommand();
+        assertEquals(Command.MOVE_RIGHT, recievedCommand);
+        hitKey(KeyCode.SPACE);
+        recievedCommand = game.getLastCommand();
+        assertEquals(Command.DROP, recievedCommand);
+    }
+    @Test public void unusedKeyPress(FxRobot robot) {
+        bindParams(robot, "");
+        Command recievedCommand;
+        hitKey(KeyCode.W);
+        recievedCommand = game.getLastCommand();
+        assertEquals(Command.ROTATE, recievedCommand);
+        recievedCommand = game.getLastCommand();
+        assertNull(recievedCommand);
+        hitKey(KeyCode.Q);
+        recievedCommand = game.getLastCommand();
+        assertNull(recievedCommand);
+        hitKey(KeyCode.ENTER);
+        recievedCommand = game.getLastCommand();
+        assertNull(recievedCommand);
+    }
 }
